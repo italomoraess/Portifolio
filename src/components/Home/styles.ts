d1a8c77a1f68f7d99dd67f5a50510e700e68670b
italomoraess <italomroraes.sth@gmail.com>
2023-04-11 18:17:17 -0300

    change proportion img home

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -3,10 +3,9 @@ import styled from "styled-components";
 export const HomeSection = styled.section`
   display: flex;
   justify-content: center;
-  padding-top: 160px;
-  padding-bottom: 110px;
   img{
     width: 1000px;
     max-width: 100%;
+    object-fit: cover;
   }
 `;
